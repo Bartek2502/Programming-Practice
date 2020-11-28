59889e1adb9dafa3eef136cdf34fbf7b60880845
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2020-11-28 10:44:59 +0000

    Added properties and a genre enum

---
+++ Week9/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9
+{
+    class Song
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public double Duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+
+
+
+    }
+
+    public enum Genre {Rock, Pop, Dance, Other}
+}
